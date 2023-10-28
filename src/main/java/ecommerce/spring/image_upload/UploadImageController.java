1ab63a9643e00deba565e0e9467ee66da4e9aa0d
sghajdao <saadghajdaoui@gmail.com>
2023-10-28 01:39:45 +0100

    add image upload

---
+++ src/main/java/ecommerce/spring/image_upload/UploadImageController.java
@@ -0,0 +1,41 @@
+package ecommerce.spring.image_upload;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/image")
+public class UploadImageController {
+
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
+
+    @GetMapping("/uploadimage")
+    public String displayUploadForm() {
+        return "imageupload/index";
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Boolean> uploadImage(Model model, @RequestParam("image") MultipartFile file)
+            throws IOException {
+        System.out.println(model);
+        System.out.println(file);
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
+        return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+    }
+}
